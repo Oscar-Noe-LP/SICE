7773b21de3e3b8d89ddcdcb3c06308d0828b0744
emili0p <emiliioizq23@gmail.com>
2026-05-31 14:57:18 -0600

    feature: primera version controladores PDF

--- Backend/config/dompdf.php
+++ Backend/config/dompdf.php
@@ -1,3 +1,6 @@
-'default_paper_size' => 'letter',
-'default_font' => 'sans-serif',
-'is_remote_enabled' => true,
+<?php
+return [
+    'default_paper_size' => 'letter',
+    'default_font' => 'sans-serif',
+    'is_remote_enabled' => true,
+];
